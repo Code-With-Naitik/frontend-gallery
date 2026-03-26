372f28033eb81f40794b741326afa861e4c19a4c
Naitik <raiyaninaitik980@gmail.com>
2026-03-26 10:30:53 +0530

    fix

--- src/url/config.js
+++ src/url/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_BASE_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:8001'
-    : 'https://api.prompt-gallery.com',
+    : import.meta.env.VITE_API_URL || 'https://frontend-gallery-beta.vercel.app' || 'https://api.prompt-gallery.com',
 };
 
 export default config;
